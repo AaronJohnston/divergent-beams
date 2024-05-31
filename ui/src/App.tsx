21662d2ab152bcd1a247f32472ff8e99c2966749
Aaron Johnston <aaron.johnston001@gmail.com>
2024-05-31 00:37:11 -0700

    Formatting and add server skeleton

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,6 +7,7 @@ function App() {
   return (
     <>
       <header className="App-header">Phuzz</header>
+      <textarea rows={30} cols={100} className="App-prompt"></textarea>
     </>
   )
 }
