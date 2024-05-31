5450bc3b6acc1270f2ea5caa9a9214da8135f4c4
Aaron Johnston <aaron.johnston001@gmail.com>
2024-05-31 01:09:26 -0700

    Generate funciton

--- server/inference.py
+++ server/inference.py
@@ -6,6 +6,12 @@ class Inference():
     def __init__(self, env: Environment):
         self.cohere = cohere.Client(env.cohere_api_key)
 
+    def generate(self, prompt: str, seed: int = 0, max_tokens=256, temperature: float = 0.8):
+        response = self.cohere.chat(
+            message=prompt, seed=seed, max_tokens=max_tokens, temperature=temperature,
+        )
+        return response
+
     def embed(self, texts: list[str]):
         response = self.cohere.embed(
             texts=texts, model="embed-english-v3.0", input_type="classification"
