aa1001a0861c7f7abae1c4871cb1927fa3338838
Aaron Johnston <aaron.johnston001@gmail.com>
2024-05-31 00:57:51 -0700

    Some server refactoring

--- server/Environment.py
+++ server/Environment.py
@@ -1,12 +1,6 @@
-from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
-class Settings(BaseSettings):
+class Environment(BaseSettings):
     cohere_api_key: str
     model_config = SettingsConfigDict(env_file=".env")
-
-
-@lru_cache
-def get_env():
-    return Settings()
--- server/inference.py
+++ server/inference.py
@@ -1,3 +1,13 @@
 import cohere
+from Environment import Environment
 
-co = cohere.Client()
+
+class Inference():
+    def __init__(self, env: Environment):
+        self.cohere = cohere.Client(env.cohere_api_key)
+
+    def embed(self, texts: list[str]):
+        response = self.cohere.embed(
+            texts=texts, model="embed-english-v3.0", input_type="classification"
+        )
+        return response
