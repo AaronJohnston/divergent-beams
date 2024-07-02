6329e8c3d272485300e2df507669cae170379c65
Aaron Johnston <aaron.johnston001@gmail.com>
2024-07-02 01:02:35 -0700

    Add demo config script

---
+++ demo/config/README.md
@@ -0,0 +1,3 @@
+# Demo Config
+
+This directory holds config files used for the demo running at https://aaronjohnston.me/divergent-beams. They are independent of the demo server but may be useful inspiration for hosting it.
---
+++ demo/config/start.sh
@@ -0,0 +1,7 @@
+source activate my_env && python -m gunicorn.app.wsgiapp \
+    --access-logfile - \
+    --worker-class uvicorn.workers.UvicornWorker \
+    --workers 1 \
+    --bind unix:/home/ec2-user/web/run/gunicorn.sock \
+    --log-level DEBUG \
+    main:app
